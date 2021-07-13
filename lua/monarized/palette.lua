f06141537e3521da6772dcd2054398aa4bd2d7c0
Jean-Hadrien Chabran <jh@chabran.fr>
2021-07-13 17:03:57 +0200

    Add gruvbox and light variants

--- lua/monarized/palette.lua
+++ lua/monarized/palette.lua
@@ -14,7 +14,7 @@ local colors = {
   accent1 = '#268bd2',
   -- highlighting (used in UI elements)
   hl0 = '#d33682',
-  hl1 = '#6c71c4',
+  hl1 = '#859900',
 
   -- normal colors (terminal, diffs, ...)
   yellow =    '#b58900',
@@ -27,35 +27,78 @@ local colors = {
   green =     '#859900',
 }
 
-if vim.g.monarized_style == 'dark_purple' then
+if vim.g.monarized_style == 'light' then
+  colors.fg0 = '#002b36'
+  colors.fg1 = '#073642'
+  colors.bg0 = '#eee8d5'
+  colors.bg1 = '#C2C2C2'
+  colors.fade0 = '#586e75'
+  colors.fade1 = '#657b83'
+  colors.accent1 = '#2aa198'
+  colors.accent0 = '#268bd2'
+  colors.hl0 = '#d33682'
+  colors.hl1 = '#859900'
+end
+
+if vim.g.monarized_style == 'purple-dark' then
   colors.bg0 = '#101129'
   colors.bg1 = '#171830'
-  colors.fg0 = '#DDE0E1'
-  colors.fg1 = '#C2C2C2'
+  colors.fg0 = '#D0C5E7'
+  colors.fg1 = '#B9A9DB'
   colors.fade0 = '#5F606B'
   colors.fade1 = '#95969C'
-  colors.accent0 = '#4B5580'
-  colors.accent1 = '#5A559E'
+  colors.accent0 = '#8A6FC3'
+  colors.accent1 = '#964BA5'
   colors.hl0 = '#826987'
   colors.hl1 = '#9185AB'
 end
 
--- -- TODO tones should be adjusted in the opposite order, ie lighter -> bg0
--- if vim.g.monarized_style == 'light' then
---   local fade1 = colors.fade1
---   local fade1 = colors.fade1
---   local fg1 = colors.fg1
---   local fg0 = colors.fg0
+if vim.g.monarized_style == 'gruvbox-dark' then
+  colors.bg0 = '#282828'
+  colors.bg1 = '#3c3836'
+  colors.fg0 = '#fbf1c7'
+  colors.fg1 = '#ebdbb2'
+  colors.fade0 = '#bdae93'
+  colors.fade1 = '#a89984'
 
---   colors.fade1 = colors.fade0
---   colors.fade1 = colors.fade0
---   colors.fg1 = colors.bg1
---   colors.fg0 = colors.bg0
+  colors.red = '#cc241d'
+  colors.green = '#98971a'
+  colors.yellow = '#d79921'
+  colors.blue = '#458588'
+  colors.magenta = '#b16286'
+  colors.violet = '#d3869b'
+  colors.cyan = '#8ec07c'
+  colors.orange = '#d65d0e'
 
---   colors.fade0 = fade1
---   colors.fade0 = fade1
---   colors.bg1 = fg1
---   colors.bg0 = fg0
--- end
+  colors.accent0 = colors.cyan
+  colors.accent1 = colors.blue
+
+  colors.hl0 = colors.violet
+  colors.hl1 = colors.yellow
+end
+
+if vim.g.monarized_style == 'gruvbox-light' then
+  colors.fg0 = '#282828'
+  colors.fg1 = '#3c3836'
+  colors.bg0 = '#fbf1c7'
+  colors.bg1 = '#ebdbb2'
+  colors.fade0 = '#7c6f64'
+  colors.fade1 = '#504945'
+
+  colors.red = '#cc241d'
+  colors.green = '#98971a'
+  colors.yellow = '#d79921'
+  colors.blue = '#458588'
+  colors.magenta = '#b16286'
+  colors.violet = '#d3869b'
+  colors.cyan = '#427b58'
+  colors.orange = '#d65d0e'
+
+  colors.accent0 = colors.cyan
+  colors.accent1 = colors.blue
+
+  colors.hl0 = colors.violet
+  colors.hl1 = colors.yellow
+end
 
 return colors
