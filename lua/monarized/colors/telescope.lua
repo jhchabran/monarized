bafeb03a068d426be7113ae283eaed0cf8a8edb6
Jean-Hadrien Chabran <jh@chabran.fr>
2021-07-18 20:46:38 +0200

    Make telescope results more readable

--- lua/monarized/colors/telescope.lua
+++ lua/monarized/colors/telescope.lua
@@ -4,7 +4,7 @@ return function(c, s, cs)
         {'TelescopeBorder', c.fade0},
         {'TelescopeNormal', c.fg1},
         {'TelescopePromptPrefix', c.accent1},
-        {'TelescopeSelection', c.fg1, c.hl1, s.bold},
-        {'TelescopeMatching', c.fg1, c.hl0, }
+        {'TelescopeSelection', c.bg1, c.hl1, s.bold},
+        {'TelescopeMatching', c.bg1, c.hl0, }
     }
 end
